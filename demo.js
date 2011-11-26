0f81bd4df5a4235b5136af179e7ef05c6db08d89
Jiayi Zheng <thebluber@googlemail.com>
2011-11-26 04:17:35 +0100

    load_css new in demo.js

--- demo.js
+++ demo.js
@@ -1,11 +1,9 @@
 //create a box
 alert("hallooooo");
 var load_css = (function (){
-  var css = document.createElement("link");
-    css.rel = "stylesheet";
-    css.href = "file:///home/jiayi/programme/jquery/wadokuBookmarklet/demo.css";
-    css.type = "text/css";
-    document.getElementsByTagName("head")[0].appendChild(css);
+  var css = document.createElement("style");
+  css.innerHTML = "ul#wadokutres {position:fixed !important;top:20px !important; right:20px !important; width:30% !important; float:right !important; background-color:#f5f5f5 !important;margin-bottom:20px !important;padding:5px !important;min-height:40px;border:1px solid #eee;border:1px solid rgba(0,0,0,0.05);-webkit-border-radius:6px;-moz-border-radius:6px;border-radius:6px;list-style-position:inside;list-style-type:none;margin-left:0;text-align:left;font-size:70%;font-family:'myriad-pro-1','myriad-pro-2','LucidaGrande',Sans-Serif;-webkit-box-shadow:0 3px 3px rgba(0,0,0,0.3);-moz-box-shadow:0 3px 3px rgba(0,0,0,0.3);box-shadow:0 3px 3px rgba(0,0,0,0.3);-webkit-background-clip:padding-box;-moz-background-clip:padding-box;text-shadow:0 1px 1px #ffffff;text-indent:4px;filter:alpha(opacity=90);-khtml-opacity:0.9;-moz-opacity:0.9;opacity:0.9;}\n ul#wadokutres li:nth-child(odd) {-webkit-border-radius:3px;-moz-border-radius:3px;border-radius:3px;border:2px solid white;background-color:#f9f9f1;padding:0;text-indent:2px;}#wadokutres li:nth-child(1){text-align:center;background-color:#fefbf1;text-indent:0px;}\ndiv.tres{text-align:center;background-color:#fefbf1;text-indent:0px;}" 
+    document.body.appendChild(css);
     alert("load css");
 });
 
