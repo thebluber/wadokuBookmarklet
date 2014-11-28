bcb02a58de78a3600690ae479b7a2b1a01565364
Jiayi Zheng <thebluber@gmail.com>
2014-11-28 14:36:34 +0100

    Add clear results button Update css style Adjust dom elements

--- demo.js
+++ demo.js
@@ -1,17 +1,31 @@
 var loadCss = function (){
   var css = document.createElement("style");
-  css.innerHTML = "ul#wadokutres {z-index:999;position:fixed !important;top:20px !important; right:20px !important; width:30% !important; float:right !important; background-color:#f5f5f5 !important;margin-bottom:20px !important;padding:5px !important;min-height:40px !important;border:1px solid #eee;border:1px solid rgba(0,0,0,0.05);-webkit-border-radius:6px;-moz-border-radius:6px;border-radius:6px;list-style-position:inside !important;list-style-type:none !important;margin-left:0 !important;text-align:left !important;font-size:70% !important;font-family:'myriad-pro-1','myriad-pro-2','LucidaGrande',Sans-Serif !important;-webkit-box-shadow:0 3px 3px rgba(0,0,0,0.3);-moz-box-shadow:0 3px 3px rgba(0,0,0,0.3);box-shadow:0 3px 3px rgba(0,0,0,0.3);-webkit-background-clip:padding-box;-moz-background-clip:padding-box;text-shadow:0 1px 1px #ffffff !important;text-indent:4px !important; filter:alpha(opacity=100);-khtml-opacity:1;-moz-opacity:1;opacity:1;}\n ul#wadokutres li:nth-child(odd) {-webkit-border-radius:3px;-moz-border-radius:3px;border-radius:3px;border:2px solid white;background-color:#f9f9f1;padding:0;text-indent:2px;}"
+  css.innerHTML = ".wadokuLabel{display:block;padding:.2em .6em .3em;font-size:150%;font-weight:700;text-align:center;vertical-align:baseline}button#clearResults{float: right}div#entries {z-index:999;position:fixed !important;background-color: #F1F2F4 !important;top:10px !important; right:10px !important; width:30% !important; float:right !important; margin-bottom:10px !important;padding:5px !important;min-height:40px !important;border:1px solid #eee;border:1px solid rgba(0,0,0,0.05);-webkit-border-radius:6px;-moz-border-radius:6px;border-radius:6px;list-style-position:inside !important;list-style-type:none !important;margin-left:0 !important;text-align:left !important;font-size:70% !important;font-family:'myriad-pro-1','myriad-pro-2','LucidaGrande',Sans-Serif !important;-webkit-box-shadow:0 3px 3px rgba(0,0,0,0.3);-moz-box-shadow:0 3px 3px rgba(0,0,0,0.3);box-shadow:0 3px 3px rgba(0,0,0,0.3);-webkit-background-clip:padding-box;-moz-background-clip:padding-box;text-shadow:0 1px 1px #ffffff !important;text-indent:4px !important; filter:alpha(opacity=90);-khtml-opacity:0.9;-moz-opacity:0.9;opacity:0.9;}\n.emph{font-weight:700}span.genus{font-weight:700;color:orange;font-size:xx-small}span.f{font-weight:700;color:red;font-size:xx-small}span.m{font-weight:700;color:#00f;font-size:xx-small}span.n{font-weight:700;color:purple;font-size:xx-small}span.dom{font-size:x-small}span.fore,span.scientif,span.title,span.transcr,span.usage{font-style:italic}span.def{color:green}#entries{font-family:'Crimson Text',arial,serif;font-size:1em;padding-top:6px}.entry .writing{font-size:1.5em}.entry p{margin:0}.entry{padding:3px 10px}.entry.odd{background:#EBEEF5}.entries .span6:nth-child(even){border-left:1px solid gray;padding-left:20px;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;box-sizing:border-box}.genki_k9,.jlpt1,.jlpt2,.jlpt3,.jlpt4,.prior_1,.prior_2,.prior_3,.prior_4{display:none}.famn{font-variant:small-caps}#entries-left:first-child{-moz-border-radius-topleft:5px;-moz-border-radius-topright:5px;-moz-border-radius-bottomright:0;-moz-border-radius-bottomleft:0;-webkit-border-radius:5px 5px 0 0;border-radius:5px 5px 0 0}"
     document.body.appendChild(css);
 }
 
 //create a box
 var makeWdkBox = function (){
-  if (document.getElementById("wadokutres") == null) {
+  if (document.getElementById("entries") == null) {
     loadCss();
-    var ul = document.createElement("ul");
-    ul.innerHTML = "Wort markieren und suchen!";
-    ul.id = "wadokutres";
-    document.body.appendChild(ul);
+    var div = document.createElement("div");
+    div.innerHTML = "<span class='wadokuLabel'>Wort markieren und suchen!</span>";
+    div.id = "entries";
+    document.body.appendChild(div);
+  }
+}
+
+var clearResults = function (){
+  var div = document.getElementById("entries");
+  div.innerHTML = "<span class='wadokuLabel'>Wort markieren und suchen!</span>";
+  //clean selectedtext
+  var sel = window.getSelection ? window.getSelection() : document.selection;
+  if (sel) {
+    if (sel.removeAllRanges) {
+      sel.removeAllRanges();
+    } else if (sel.empty) {
+      sel.empty();
+    }
   }
 }
 
@@ -39,26 +53,44 @@ var search = function (w, callback) {
 
 
 var renderResult = function (results) {
-  var ul = document.getElementById("wadokutres");
-  ul.innerHTML = "";
+  var div = document.getElementById("entries");
+  div.innerHTML = "";
   if(results.entries.length == 0) {
-    var li = document.createElement("li");
-    li.innerHTML = "Nichts gefunden!";
-    ul.appendChild(li);
+    div.innerHTML = "<span id='notFound'>Nichts gefunden!</span>";
   } else {
+    var button = document.createElement("button");
+    button.id = "clearResults";
+    button.innerHTML = "X"
+    button.onclick = clearResults;
+    div.appendChild(button);
     var reg = /\[[a-z0-9]*\]/g;
     results.entries.sort(function(a, b){
       var midashigoA, midashigoB;
       midashigoA = a.midashigo.replace(reg, "").trim();
       midashigoB = b.midashigo.replace(reg, "").trim();
       return midashigoA.length - midashigoB.length;
-    }).forEach(function(entry){
-      var li = document.createElement("li");
-      li.innerHTML = "<a target='_blank' href='http://www.wadoku.eu/entries/by-daid/" + entry.wadoku_id + "'>" + entry.midashigo + "</a>" + "| " + entry.definition;
-      ul.appendChild(li);
+    }).slice(0,6).forEach(function(entry, index){
+      var entryDiv = document.createElement("div");
+      entryDiv.className = "entry";
+      if (index % 2 != 0) entryDiv.className += " odd";
+      var p = document.createElement("p");
+
+      var writing = document.createElement("span");
+      writing.className = "writing";
+      writing.innerHTML = "<ruby><rb>" + entry.midashigo + "</rb><rt>" + entry.furigana + "</rt></ruby> ";
+
+      p.appendChild(writing);
+      p.innerHTML += entry.definition;
+      entryDiv.appendChild(p);
+
+      entryDiv.onclick = function (){
+        var url = "http://www.wadoku.eu/entries/by-daid/" + entry.wadoku_id;
+        window.open(url,'_blank');
+      }
+      div.appendChild(entryDiv);
     });
   }
-  return ul;
+  return div;
 }
 
 
@@ -69,13 +101,7 @@ window.onload = function () {
   b.onclick = function () {
     var selectedText = selectWdk().replace(/[\s、「」、。\(\)\[\]\t（）]/g, "");
     var reg = /[\s\d\t]/g;
-    if(reg.test(selectedText) || selectedText.length > 10 || selectedText == "") {
-      var ul = document.getElementById("wadokutres");
-      ul.innerHTML = "";
-      var li = document.createElement("li");
-      li.innerHTML = "Nichts gefunden!";
-      ul.appendChild(li);
-    } else {
+    if(!reg.test(selectedText) && selectedText.length < 10 && selectedText.trim() != "") {
       search(selectedText, 'renderResult');
     };
   }
