1772779e6233ff9734a7539c4c8cbf25c7340165
Jiayi Zheng <thebluber@gmail.com>
2014-11-29 13:31:07 +0100

    Add search form

--- demo.js
+++ demo.js
@@ -1,6 +1,6 @@
 var loadCss = function (){
   var css = document.createElement("style");
-  css.innerHTML = ".wadokuLabel{display:block;padding:.2em .6em .3em;font-size:150%;font-weight:700;text-align:center;vertical-align:baseline}button#clearResults{float: right}div#entries {z-index:999;position:fixed !important;background-color: #F1F2F4 !important;top:10px !important; right:10px !important; width:30% !important; float:right !important; margin-bottom:10px !important;padding:5px !important;min-height:40px !important;border:1px solid #eee;border:1px solid rgba(0,0,0,0.05);-webkit-border-radius:6px;-moz-border-radius:6px;border-radius:6px;list-style-position:inside !important;list-style-type:none !important;margin-left:0 !important;text-align:left !important;font-size:70% !important;font-family:'myriad-pro-1','myriad-pro-2','LucidaGrande',Sans-Serif !important;-webkit-box-shadow:0 3px 3px rgba(0,0,0,0.3);-moz-box-shadow:0 3px 3px rgba(0,0,0,0.3);box-shadow:0 3px 3px rgba(0,0,0,0.3);-webkit-background-clip:padding-box;-moz-background-clip:padding-box;text-shadow:0 1px 1px #ffffff !important;text-indent:4px !important; filter:alpha(opacity=90);-khtml-opacity:0.9;-moz-opacity:0.9;opacity:0.9;}\n.emph{font-weight:700}span.genus{font-weight:700;color:orange;font-size:xx-small}span.f{font-weight:700;color:red;font-size:xx-small}span.m{font-weight:700;color:#00f;font-size:xx-small}span.n{font-weight:700;color:purple;font-size:xx-small}span.dom{font-size:x-small}span.fore,span.scientif,span.title,span.transcr,span.usage{font-style:italic}span.def{color:green}#entries{font-family:'Crimson Text',arial,serif;font-size:1em;padding-top:6px}.entry .writing{font-size:1.5em}.entry p{margin:0}.entry{padding:3px 10px}.entry.odd{background:#EBEEF5}.entries .span6:nth-child(even){border-left:1px solid gray;padding-left:20px;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;box-sizing:border-box}.genki_k9,.jlpt1,.jlpt2,.jlpt3,.jlpt4,.prior_1,.prior_2,.prior_3,.prior_4{display:none}.famn{font-variant:small-caps}#entries-left:first-child{-moz-border-radius-topleft:5px;-moz-border-radius-topright:5px;-moz-border-radius-bottomright:0;-moz-border-radius-bottomleft:0;-webkit-border-radius:5px 5px 0 0;border-radius:5px 5px 0 0}"
+  css.innerHTML = ".wadokuLabel{display:block;padding:.2em .6em .3em;font-size:150%;font-weight:700;text-align:center;vertical-align:baseline}button#clearResults{float: right}div#entries {z-index:999;position:fixed !important;background-color: #F1F2F4 !important;top:10px !important; right:10px !important; width:30% !important; float:right !important; margin-bottom:10px !important;padding:5px !important;min-height:40px !important;border:1px solid #eee;border:1px solid rgba(0,0,0,0.05);-webkit-border-radius:6px;-moz-border-radius:6px;border-radius:6px;list-style-position:inside !important;list-style-type:none !important;margin-left:0 !important;text-align:left !important;font-size:70% !important;font-family:'myriad-pro-1','myriad-pro-2','LucidaGrande',Sans-Serif !important;-webkit-box-shadow:0 3px 3px rgba(0,0,0,0.3);-moz-box-shadow:0 3px 3px rgba(0,0,0,0.3);box-shadow:0 3px 3px rgba(0,0,0,0.3);-webkit-background-clip:padding-box;-moz-background-clip:padding-box;text-shadow:0 1px 1px #ffffff !important;text-indent:4px !important; filter:alpha(opacity=90);-khtml-opacity:0.9;-moz-opacity:0.9;opacity:0.9;}\n.emph{font-weight:700}span.genus{font-weight:700;color:orange;font-size:xx-small}span.f{font-weight:700;color:red;font-size:xx-small}span.m{font-weight:700;color:#00f;font-size:xx-small}span.n{font-weight:700;color:purple;font-size:xx-small}span.dom{font-size:x-small}span.fore,span.scientif,span.title,span.transcr,span.usage{font-style:italic}span.def{color:green}#entries{font-family:'Crimson Text',arial,serif;font-size:1em;padding-top:6px}.entry .writing{font-size:1.5em}.entry p{margin:0}.entry{padding:3px 10px}.entry.odd{background:#EBEEF5}.entries .span6:nth-child(even){border-left:1px solid gray;padding-left:20px;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;box-sizing:border-box}.genki_k9,.jlpt1,.jlpt2,.jlpt3,.jlpt4,.prior_1,.prior_2,.prior_3,.prior_4{display:none}.famn{font-variant:small-caps}#entries-left:first-child{-moz-border-radius-topleft:5px;-moz-border-radius-topright:5px;-moz-border-radius-bottomright:0;-moz-border-radius-bottomleft:0;-webkit-border-radius:5px 5px 0 0;border-radius:5px 5px 0 0}input#searchInWadoku{margin-right:20px;display:inline-block;vertical-align:middle;padding-left:14px;padding-right:14px;border:1px solid #ccc;-webkit-box-shadow:inset 0 1px 1px rgba(0,0,0,.075);-moz-box-shadow:inset 0 1px 1px rgba(0,0,0,.075);box-shadow:inset 0 1px 1px rgba(0,0,0,.075);-webkit-transition:border linear .2s,box-shadow linear .2s;-moz-transition:border linear .2s,box-shadow linear .2s;-o-transition:border linear .2s,box-shadow linear .2s;transition:border linear .2s,box-shadow linear .2s;height:20px;font-size:14px}"
     document.body.appendChild(css);
 }
 
@@ -90,7 +90,46 @@ var renderResult = function (results) {
       div.appendChild(entryDiv);
     });
   }
+
+  //Render search form
+  var form = document.createElement("form");
+  form.className = "entry";
+  var input = document.createElement("input");
+  input.type = "text";
+  input.id = "searchInWadoku";
+  input.value = results.query;
+  var btn = document.createElement("button");
+  btn.type = "submit";
+  btn.innerHTML = "In WadokuJT suchen!";
+  form.appendChild(input);
+  form.appendChild(btn);
+  form.onsubmit = function (){
+    var url = "http://www.wadoku.eu?query=" + results.query;
+    window.open(url,'_blank');
+    return false;
+  }
+  div.appendChild(form);
   return div;
+}
+
+var renderSearchInput = function (selectedtext) {
+  var div = document.getElementById("entries");
+  var form = document.createElement("form");
+  form.className = "entry";
+  var input = document.createElement("input");
+  input.type = "text";
+  input.id = "searchInWadoku";
+  input.value = selectedtext;
+  var btn = document.createElement("button");
+  btn.type = "submit";
+  btn.innerHTML = "Suchen!";
+  form.appendChild(input);
+  form.appendChild(btn);
+  form.onsubmit = function (){
+    var url = "http://www.wadoku.eu?query=" + selectedtext;
+    window.open(url,'_blank');
+  }
+  div.appendChild(form);
 }
 
 
